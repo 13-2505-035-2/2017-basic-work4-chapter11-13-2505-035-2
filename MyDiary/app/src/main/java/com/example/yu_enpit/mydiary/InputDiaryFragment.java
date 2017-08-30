2ddc615925e02dbd4b0ddeb854ae05bc7afe6660
enpit028 <t035ff@yamaguchi-u.co.jp>
2017-08-30 14:58:25 +0900

    修正

--- MyDiary/app/src/main/java/com/example/yu_enpit/mydiary/InputDiaryFragment.java
+++ MyDiary/app/src/main/java/com/example/yu_enpit/mydiary/InputDiaryFragment.java
@@ -96,6 +96,7 @@ public class InputDiaryFragment extends Fragment {
                 });
             }
         });
+
         mBodyEdit.addTextChangedListener(new TextWatcher(){
             @Override
             public void beforeTextChanged(CharSequence s,int start,int count,int after){}
